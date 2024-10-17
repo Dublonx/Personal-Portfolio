12cbf92796e5b6a0399408221a4586834975ad9f
Dublonx <dublonjm17@gmail.com>
2024-10-17 11:14:31 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// sidenav
+const menu = document.getElementById('bars');
+const xmark = document.getElementById('xmark');
+const sideNav = document.getElementById('sidenav');
+const links = document.querySelectorAll('.navLink');
+
+menu.addEventListener("click", () => {
+    sideNav.classList.add("active");
+});
+
+xmark.addEventListener("click", () => {
+    sideNav.classList.remove("active");
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        sideNav.classList.remove("active");
+    });
+});
+
+// navbar background color
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = "#024CAA";
+    } else {
+        navbar.style.backgroundColor = "#6A9AB0";
+    }
+});
+
+// typejs
+let type = new Typed('#text', {
+    strings: ["Web Developer", "Video Editor", "Web Designer"],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+});
+
+// button top
+window.addEventListener("scroll", () => {
+    const button = document.querySelector('#top');
+
+    if (window.scrollY > 50) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
